e1dd86f28d45745ddd8ba87f7b60ff48526894a9
davidngug <davidkinyanjui245@gmail.com>
2023-06-17 12:06:04 -0700

    update4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 /*program use puts to print character without printf*/
-int main(){/*tells program to start main ()*/
-	char hp[]= "Programming is like building a multilingual puzzle\n";
+int main(void)
+
+{/*tells program to start main ()*/
+	char hp[] = "Programming is like building a multilingual puzzle\n";
+
 	puts(hp);
-	return (0)/*this etells code completed without error*/
+	return (0);/*this etells code completed without error*/
+
 }
