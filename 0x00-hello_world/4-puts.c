243803915251c8503f4e6899ec52c6799fbde849
davidngug <davidkinyanjui245@gmail.com>
2023-06-17 12:22:47 -0700

    update_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /*program use puts to print character without printf*/
 int main(void)
 {
-	puts("\ "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);/*this etells code completed without error*/
 
 }
