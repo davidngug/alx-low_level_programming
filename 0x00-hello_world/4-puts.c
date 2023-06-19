99a05dfa04863d3493fe32023639d4b799e3aabc
davidngug <davidkinyanjui245@gmail.com>
2023-06-19 15:55:00 -0700

    update4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-/*program use puts to print character without printf*/
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);/*this etells code completed without error*/
+	return (0);
 
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -aprogramme that ensures function prinf is used
+ * return: (0) -success without error
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
