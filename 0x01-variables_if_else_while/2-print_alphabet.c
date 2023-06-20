c47cd68df800fbe188f043cd53325edd161b7246
davidngug <davidkinyanjui245@gmail.com>
2023-06-19 17:32:41 -0700

    update 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
